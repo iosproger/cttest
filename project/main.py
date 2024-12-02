d8e278a68571572eff916b76d3ab77f9d40add6e
Abdurashidbek Sadullayev <135204006+iosproger@users.noreply.github.com>
2024-12-02 15:16:31 +0000

    update

--- project/main.py
+++ project/main.py
@@ -37,8 +37,11 @@ async def root():
 # if __name__ == '__main__':
 #     uvicorn.run("main:app", reload=True)
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True, host="0.0.0.0")
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", reload=True, host="0.0.0.0")
+
+# python -m uvicorn main:app --host 0.0.0.0 --port 8001
+
 
 # uvicorn main:app --host 0.0.0.0 --port 8001
 # pip install uvicorn
