033b709d77f1deb543aab039791b707c294a224e
Abdurashidbek Sadullayev <135204006+iosproger@users.noreply.github.com>
2024-12-03 13:28:28 +0000

    update

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 # PYTHONPATH=/workspaces/cttest/project python -m uvicorn main:app --host 0.0.0.0 --port 8001
 # from ..schemas.schemas import TaskType
+# PYTHONPATH=/workspaces/cttest/project python -m uvicorn main:app --host 0.0.0.0 --port 8001
